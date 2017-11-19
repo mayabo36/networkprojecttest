1133ae489f1e1e0ca97737e13516330781c79794
Rebeca Amaya <rebeca_amaya@bellsouth.net>
2017-11-19 13:37:03 -0500

    Done

--- src/Recommender.java
+++ src/Recommender.java
@@ -15,8 +15,8 @@ public class Recommender {
     static int N = 2;
 
     // row and column count for rating matrix
-    static int itemCount = 6;
-    static int userCount = 12;
+    static int itemCount;
+    static int userCount;
 
     // Toy rating matrix example
     // Rows represent items and columns represent users
@@ -25,7 +25,7 @@ public class Recommender {
     static ArrayList<ArrayList<Double>> ratings = new ArrayList<>();
 
     // Array list for row means
-    static double [] itemRatingMean  = new double [itemCount];
+    static double [] itemRatingMean;
 
     /*
         Normalize ratings by subtracting row mean.
@@ -80,9 +80,8 @@ public class Recommender {
 
     /*
         Calculate the centered cosine similarity matrix
-        TODO: rename ratings to normalizedMatrix
      */
-    private static ArrayList<ArrayList<Double>> centeredCosineSimilarity(ArrayList<ArrayList<Double>> ratings){
+    private static ArrayList<ArrayList<Double>> centeredCosineSimilarity(ArrayList<ArrayList<Double>> normalizedRatings){
 
         ArrayList<ArrayList<Double>> ccsMatrix = new ArrayList<>();
         for(int i = 0; i < itemCount; i++)
@@ -95,12 +94,12 @@ public class Recommender {
                 dotProduct = item1Magnitude = item2Magnitude= 0.0;
 
                 for (int user = 0; user < userCount; user++) {
-                    dotProduct += ratings.get(item1).get(user) * ratings.get(item2).get(user);
-                    item1Magnitude += ratings.get(item1).get(user) * ratings.get(item1).get(user);
-                    item2Magnitude += ratings.get(item2).get(user) * ratings.get(item2).get(user);
+                    dotProduct += normalizedRatings.get(item1).get(user) * normalizedRatings.get(item2).get(user);
+                    item1Magnitude += normalizedRatings.get(item1).get(user) * normalizedRatings.get(item1).get(user);
+                    item2Magnitude += normalizedRatings.get(item2).get(user) * normalizedRatings.get(item2).get(user);
                 }
-
-                similarity = dotProduct / (Math.sqrt(item1Magnitude) * Math.sqrt(item2Magnitude));
+                double denominator = (Math.sqrt(item1Magnitude) * Math.sqrt(item2Magnitude));
+                similarity = (denominator != 0 ? dotProduct / denominator : 0.0); // check that we do not divide by 0
                 ccsMatrix.get(item1).add(similarity);
             }
         }
@@ -110,40 +109,61 @@ public class Recommender {
 
     /*
         Implement item-item collaborative filter to estimate missing ratings for items
-        TODO: finish this
      */
     private static void item_itemCollaborativeFilter(ArrayList<ArrayList<Double>> ccsMatrix){
 
-        ArrayList<ArrayList<Integer>> itemNeigborhood = new ArrayList<>();
-        for(int user = 0; user < userCount; user++){
+        DecimalFormat df = new DecimalFormat("####0.00");
 
-            ArrayList<Integer> neighborhood = new ArrayList<>();
+        // Get item neighborhood for all items in descending order based on similarity
+        ArrayList<ArrayList<Item>> itemNeighborhood = new ArrayList<>();
+        int i = 0;
+        for(ArrayList<Double> items : ccsMatrix){
 
-            for(int item = 0; item < itemCount; item++){
-                if(ratings.get(item).get(user) != 0){
-                    neighborhood.add(item);
-                }
+            ArrayList<Item> neighborhood = new ArrayList<>();
+
+            for(int similarity = 0; similarity < items.size(); similarity++){
+                // Don't add item to its own neighborhood
+                if(similarity != i)
+                    neighborhood.add(new Item(similarity, items.get(similarity)));
             }
-            itemNeigborhood.add(neighborhood);
+
+            // Sort the item neighborhood
+            Collections.sort(neighborhood, new CustomComparator());
+            Collections.reverse(neighborhood);
+            itemNeighborhood.add(neighborhood);
+
+            i++;
         }
+        System.out.println("\n");
 
 
+        // summations of similarity;
+        double predictedRating, similarityByRating, similarity;
+
+        // Traverse ratings matrix and estimate missing ratings
         for(int item = 0; item < itemCount; item++){
             for(int user = 0; user < userCount; user++){
 
-                // Estimate rating for items that user has not yet rated
+                // If we have a missing rating
                 if(ratings.get(item).get(user) == 0){
-                    ArrayList<Item> items = new ArrayList<>();
-                    for(int i = 0; i < itemNeigborhood.get(user).size(); i++){
-                        items.add(new Item(itemNeigborhood.get(user).get(i), ccsMatrix.get(item).get(i)));
-                    }
-                    Collections.sort(items, new CustomComparator());
-                    Collections.reverse(items);
 
-                    for(int i = 0; i < itemNeigborhood.get(user).size(); i++){
-                        System.out.print(items.get(i).similarity + " ");
+                    // reset summations of similarity
+                    predictedRating = similarityByRating = similarity = 0.0;
+
+                    // Pick the top N most similar items to the current item that have been rated by the current user
+                    int n = 0;
+                    for(Item in : itemNeighborhood.get(item)){
+                        if(ratings.get(in.item).get(user) != 0.0){
+                            similarityByRating += (in.similarity) * ratings.get(in.item).get(user);
+                            similarity += (in.similarity);
+                            n++;
+                        }
+                        if(n == N) break;
                     }
-                    System.out.println();
+
+                    predictedRating = (similarity != 0 ? similarityByRating / similarity : 0.0);
+
+                    ratings.get(item).set(user, (predictedRating < 1.0 ? 1.0 : (predictedRating > 5.0 ? 5.0 : predictedRating)));
                 }
             }
         }
@@ -159,6 +179,10 @@ public class Recommender {
         ratings.add(new ArrayList<Double>(Arrays.asList(0.0, 2.0, 4.0, 0.0, 5.0, 0.0, 0.0, 4.0, 0.0, 0.0, 2.0, 0.0)));
         ratings.add(new ArrayList<Double>(Arrays.asList(0.0, 0.0, 4.0, 3.0, 4.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 5.0)));
         ratings.add(new ArrayList<Double>(Arrays.asList(1.0, 0.0, 3.0, 0.0, 3.0, 0.0, 0.0, 2.0, 0.0, 0.0, 4.0, 0.0)));
+
+        itemCount = ratings.size();
+        userCount = ratings.get(0).size();
+        itemRatingMean = new double[itemCount];
 
         System.out.println("Initial matrix:\n");
         printMatrix(ratings);
@@ -176,7 +200,10 @@ public class Recommender {
         printMatrix(ccsMatrx);
 
         // Calculate item-item collaborative filtering to estimate ratings
-       // item_itemCollaborativeFilter(ccsMatrx);
+        item_itemCollaborativeFilter(ccsMatrx);
+
+        System.out.println("Predicted Ratings Matrix:\n");
+        printMatrix(ratings);
     }
 
 }
